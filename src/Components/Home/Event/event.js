418103278e4811681999c39539edcd4717d48d6d
Kukesan <kkukesan@gmail.com>
2024-02-02 15:24:42 +0530

    home page responsive added

--- src/Components/Home/Event/event.js
+++ src/Components/Home/Event/event.js
@@ -136,12 +136,12 @@ function Event() {
                     <div className="intro-heading1">தமிழர் தேசம் எங்கிலும் பரந்து வாழும் பாடசாலை மாணவர்களின் பல்முக திறமைகளுக்கு களம் அமைத்து அகில இலங்கைரீதியில் அந்த சாதனை வீர்ர்களுக்கு அங்கீகாரத்தை வழங்க தமிழ் இலக்கிய மன்றத்தால் பெருமையுடன் நடாத்தபடும் போட்டிகள்</div>
                     <Grid container
                         spacing={4}
-                        columns={16}
+                        // columns={16}
                         direction="row"
                         justifyContent="center"
                         alignItems="center">
                         {competiton.map((event, index) => <>
-                            <Grid item xs={5}>
+                            <Grid item xl='4' lg='4' md='4' sm='6' xs='12' key={index}>
                                 <Link to={event.link}>
                                     <div className="event-card">
                                         <div className="event-card-top">
@@ -163,13 +163,12 @@ function Event() {
                     <div className="intro-heading1">தாங்கள் கடந்து வந்த அனுபவங்களையும் படிப்பினைகளையும் தமது தம்பி, தங்கைகளுக்கும் கூறி அவர்களை வழிநடத்தி அவர்களின் இலக்குகளை அடைய கைகோர்க்கும் சிரேஷ்ட மாணவர்களுக்கும் பல்கலைக்கழக மாணவர்களுக்குமான உறவுப்பாலம்</div>
                     <Grid container
                         spacing={4}
-                        columns={16}
                         direction="row"
                         justifyContent="center"
                         alignItems="center"
                         >
                         {guidence.map((event, index) => <>
-                            <Grid item xs={5}>
+                            <Grid item xl='4' lg='4' md='3' sm='6' xs='12' key={index}>
                                 <Link to={event.link}>
                                     <div className="event-card">
                                         <div className="event-card-top">
@@ -195,6 +194,7 @@ function Event() {
                         justifyContent="space-around"
                         alignItems="center"
                         spacing={4}
+                        
                     >
                         {social.map((event, index) => <>
                             <Grid item xl='4' lg='4' md='4' sm='6' xs='12' key={index} >
@@ -217,12 +217,11 @@ function Event() {
                     <div className="intro-heading1">பல்கலையில் பயிலும் தமிழ் மாணவர்களின் கலைத்திறமைகளை வெளி உலகிற்கு பறைசாற்றிடும் நிகழ்வுகள் பல்கலையில் பயிலும் தமிழ் மாணவர்களின் கலைத்திறமைகளை வெளி உலகிற்கு பறைசாற்றிடும் நிகழ்வுகள்</div>
                     <Grid container
                         spacing={4}
-                        columns={16}
                         direction="row"
                         justifyContent="center"
                         alignItems="center">
                         {carnival.map((event, index) => <>
-                            <Grid item xs={5}>
+                            <Grid item xl='4' lg='4' md='4' sm='6' xs='12' key={index}>
                                 <Link to={event.link}>
                                     <div className="event-card">
                                         <div className="event-card-top">
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -27,7 +27,7 @@ function Home() {
             <Intro />
             <Event />
             <Gallery />
-            <Timeline />
+            {/* <Timeline /> */}
             <Contact />
         </>)
 }
