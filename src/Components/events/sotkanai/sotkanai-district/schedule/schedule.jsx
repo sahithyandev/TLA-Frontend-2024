014c589ef76882383f5dcde2ca8db30f7149989d
tha-ree-jan <thareejap.20@uom.lk>
2024-02-05 17:31:40 +0530

    district-page-init

---
+++ src/Components/events/sotkanai/sotkanai-district/schedule/schedule.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./schedule.css";
+import { Container } from "@mui/material";
+// import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+// import AccessTimeIcon from "@mui/icons-material/AccessTime";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
+// import { CalendarMonth,AccessTime,LocationOn } from "@mui/icons-material";
+const Schedule = () => {
+  return (
+    <React.Fragment>
+      <div className="sotkanai-landing-container-div">
+        <Container maxWidth="" className="sotkanai-landing-container">
+          <div className="sotkanai-district-landing-heading1">
+            சொற்கணை-யாழ்ப்பாண மாவட்டம்
+          </div>
+          <div className="head-landing-heading1">சொற்கணை-2024</div>
+        </Container>
+      </div>
+
+        <div className="schedule-container">
+        dkbcfawhkjfgh
+        </div>
+    </React.Fragment>
+  );
+};
+
+export default Schedule;
