f7f465580043ba413b182fe78a9043b144c3531d
Thanusiyan Sivapalasundharam <thanusiyans@Thanusiyans-MacBook-Pro.local>
2024-02-20 23:08:15 +0530

    responsive added for Aramiyam page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Router from './Router'
-import Navbar from './Components/Home/Navbar/navbar';
-import Footer from './Components/Home/Footer/footer';
-import ScrollToTop from './Components/ScrollToTop';
-import { useEffect } from 'react';
-import Aramiyam from './Pages/Aramiyam';
+import Router from "./Router";
+import Navbar from "./Components/Home/Navbar/navbar";
+import Footer from "./Components/Home/Footer/footer";
+import ScrollToTop from "./Components/ScrollToTop";
+import { useEffect } from "react";
+import Aramiyam from "./Pages/Aramiyam";
 
 function App() {
-  useEffect(() => {
-
-  }, [])
+  useEffect(() => {}, []);
   return (
     <>
       <ScrollToTop />
       <Navbar />
-      <Router />
+      {/* <Router /> */}
+      <Aramiyam />
       <Footer />
     </>
   );
