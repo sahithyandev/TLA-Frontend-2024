b2b917230ad47b6bc0a9ef11b82ccc75ec3d15fd
Kukesan <kkukesan@gmail.com>
2024-04-14 00:26:58 +0530

    aramiyam api connected

--- src/Components/events/aramiyam/seminar/seminarCard/SeminarCard.js
+++ src/Components/events/aramiyam/seminar/seminarCard/SeminarCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./seminarcard.css";
 import youtubeImg from "../../../../../images/Events/aramiyam/Youtube.png";
+import profileImg01 from "../../../../../images/Events/aramiyam/profile01.png";
 import {
   Container,
   Grid,
@@ -93,7 +94,7 @@ function SeminarCard({ SeminarData }) {
             xs={12}
             className="profile-frame"
           >
-            <img className="profile-img" src={SeminarData.speaker.profileImg} />
+            <img className="profile-img" src={SeminarData.speaker.profileImg != '' ? SeminarData.speaker.profileImg :profileImg01} />
             <Grid
               display={"flex"}
               flexDirection={{ xs: "column", md: "row" }}
