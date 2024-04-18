dd9533797d2dc56ed355dede04ef3df8017fd29d
Gayathri-Sivakumar <sivakumarg.19@uom.lk>
2024-04-18 18:25:00 +0530

    Logo adjusted

--- src/Components/Home/Navbar/navbar.js
+++ src/Components/Home/Navbar/navbar.js
@@ -1,25 +1,27 @@
-import * as React from 'react';
-import { Link, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as React from "react";
+import { Link, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { Container } from '@mui/material';
-import './navbar.css'
-import Logo from '../../../images/Logo-Nav/logo.png'
-import { NavLink } from 'react-router-dom';
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Container } from "@mui/material";
+import "./navbar.css";
+import Logo from "../../../images/Logo-Nav/logo.png";
+import { NavLink } from "react-router-dom";
+import { colors } from "material-ui/styles";
+import { Padding } from "@mui/icons-material";
 const drawerWidth = 240;
 
 // const navItems = [{
@@ -27,26 +29,28 @@ const drawerWidth = 240;
 //   link:'/'
 // }]
 
-const navItems = [{
-  name: 'முகப்பு',
-  link: '/'
-},
-{
-  name: 'வரலாறு',
-  link: '/history'
-},
-{
-  name: 'நூல்கள்',
-  link: '/books'
-},
-{
-  name: 'அணிகள்',
-  link: '/teams'
-},
-{
-  name: 'தொடர்புகள்',
-  link: '/contact'
-}];
+const navItems = [
+  {
+    name: "முகப்பு",
+    link: "/",
+  },
+  {
+    name: "வரலாறு",
+    link: "/history",
+  },
+  {
+    name: "நூல்கள்",
+    link: "/books",
+  },
+  {
+    name: "அணிகள்",
+    link: "/teams",
+  },
+  {
+    name: "தொடர்புகள்",
+    link: "/contact",
+  },
+];
 
 function Navbar(props) {
   const { window } = props;
@@ -57,18 +61,18 @@ function Navbar(props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }} className='nav-Items'>
-        <Link to="/" style={{ display: 'flex', justifyContent: 'center' }}>
-          <img src={Logo} alt='' height={50} />
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }} className="nav-Items">
+        <Link to="/" style={{ display: "flex", justifyContent: "center" }}>
+          <img src={Logo} alt="" height={50} />
         </Link>
       </Typography>
       <Divider />
       <List>
         {navItems.map((item) => (
-          <ListItem key={item.name} disablePadding >
-            <ListItemButton sx={{ textAlign: 'center' }} >
-              <ListItemText >
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
                 <Link to={item.link} className="nav-Items">
                   {item.name}
                 </Link>
@@ -80,36 +84,47 @@ function Navbar(props) {
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex', height: '64px' }} className='navbar-container'>
-      <AppBar component="nav" className='appBar'>
-        <Container maxWidth='xl' >
-          <Toolbar sx={{ height: '64px' }}>
+    <Box sx={{ display: "flex", height: "64px" }} className="navbar-container">
+      <AppBar component="nav" className="appBar">
+        <Container maxWidth="xl">
+          <Toolbar sx={{ height: "64px" }}>
             <IconButton
               color="inherit"
               aria-label="open drawer"
               edge="start"
               onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' }, color: "#022345" }}
+              sx={{ mr: 2, display: { sm: "none" }, color: "#022345" }}
             >
               <MenuIcon />
             </IconButton>
             <Typography
               variant="h6"
               component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-              className='nav-Items'
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+              className="nav-Items"
             >
-              <Link to="/" style={{ display: 'flex' }}>
-                <img src={Logo} alt='' height={50} />
+              <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src={Logo}
+                  alt=""
+                  height={50}
+                  style={{ marginRight: "10px" }}
+                />
+                <p>மொறட்டுவைப் பல்கலைக்கழக தமிழ் இலக்கிய மன்றம்</p>
               </Link>
-
             </Typography>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
               {navItems.map((item) => (
-                <NavLink key={item.name} to={item.link} className='nav-Items' activeClassName="active-nav-Item">
+                <NavLink
+                  key={item.name}
+                  to={item.link}
+                  className="nav-Items"
+                  activeClassName="active-nav-Item"
+                >
                   {item.name}
                 </NavLink>
               ))}
@@ -127,8 +142,11 @@ function Navbar(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -138,7 +156,6 @@ function Navbar(props) {
         <Toolbar />
       </Box>
     </Box>
-
   );
 }
 
