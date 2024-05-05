54cd8cb80da32fec2b21fcbd0b75cec7ae66ea38
Raguraj Sivanantham <99555816+sragu2000@users.noreply.github.com>
2024-05-05 16:21:32 +0530

    Brammam API Connected

--- src/Components/events/brammam/rules/BrammamRules.js
+++ src/Components/events/brammam/rules/BrammamRules.js
@@ -1,39 +1,27 @@
-import React from 'react';
-import './BrammamRules.css';
-import { Grid, Container, Button } from '@mui/material';
-import { useState } from 'react';
-import Heading from '../../../../shared/Heading';
+import React from "react";
+import "./BrammamRules.css";
+import { Grid, Container } from "@mui/material";
+import Heading from "../../../../shared/Heading";
 const BrammamRules = ({ eventData }) => {
-    const [showRules, setShowRules] = useState(false);
+  return (
+    <React.Fragment>
+      <div>
+        <Heading>{eventData?.nameTamil} - போட்டி விதிமுறைகள்</Heading>
 
-    const handleButtonClick = () => {
-        setShowRules(!showRules);
-    };
-    return (<React.Fragment>
-        <div>
-            <Heading>
-                {eventData["name"]} - போட்டி விதிமுறைகள்
-            </Heading>
-
-
-            <Grid item sm='12'>
-                <Container maxWidth="md" >
-                    <div className="rules-decription">
-                        <ol type='1'>
-                            {
-                                eventData["rules"].map((rule, index) => {
-                                    return <li key={index}>{rule}</li>
-                                })
-                            }
-                        </ol>
-                    </div>
-
-                </Container>
-            </Grid>
-
-
-        </div>
-    </React.Fragment>);
-}
+        <Grid item sm="12">
+          <Container maxWidth="md">
+            <div className="rules-decription">
+              <ol type="1">
+                {eventData?.rules?.map((rule, index) => {
+                  return <li key={index}>{rule}</li>;
+                })}
+              </ol>
+            </div>
+          </Container>
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default BrammamRules;
